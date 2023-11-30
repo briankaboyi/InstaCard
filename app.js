f4f650de6f4cb20bea14b46c29c592f071485dc4
briankaboyi <briankaboyi0@gmail.com>
2023-11-30 10:34:34 +0300

    implemented local storage/css fixes

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-const heart = document.getElementsByClassName('card')
-container = document.getElementById('container')
-console.log(heart,container)
+const heart = document.getElementsByClassName("card");
+container = document.getElementById("container");
+let initialLikes = localStorage.getItem("likes");
+container.querySelector(".likes-count-child").textContent = initialLikes
+container.addEventListener("click", (e) => {
+  let clickedElement = e.target;
+  let countElement =
+    clickedElement.parentElement.parentElement.querySelector(".like-count");
+    let countElementChild = 
+    clickedElement.parentElement.parentElement.querySelector(
+      ".likes-count-child"
+    );
 
-container.addEventListener("click",(e)=>{
-    let clickedElement = e.target
-    let countElement = clickedElement.parentElement.parentElement.querySelector('.like-count')
-    console.log(countElement)
-    console.log(clickedElement.className)
-    
+  if (clickedElement.className == "fa-solid fa-heart") {
+    clickedElement.style.color = "red";
 
-    if(clickedElement.className == 'fa-solid fa-heart'){
-        clickedElement.style.color = 'red'
-        let count = +countElement.childElement.textContent + 1 || 0;
-        countElement.innerText =`${count} likes` ;
-        console.log(count)
+    if (localStorage.getItem("likes") == null) {
+      ++countElementChild.textContent;
+      localStorage.setItem("likes", countElementChild.textContent);
+    } else {
+      let localValue = localStorage.getItem("likes");
+      localStorage.setItem("likes", ++localValue);
+      countElementChild.textContent = localStorage.getItem("likes");
     }
-
-
-})
-
-let  likesObj = {
-    likes:0
-}
-likesObjSerialized = JSON.stringify(likesObj)
-localStorage.setItem("likesObj",likesObjSerialized)
-console.log(likesObjSerialized)
-let likesObjDerialized = JSON.parse(localStorage.getItem('likesObj'))
-console.log(likesObjDerialized)
+  }
+});
