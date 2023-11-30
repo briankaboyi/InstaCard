d7baaec47ccf801f97685ef6650b15b830debb42
briankaboyi <briankaboyi0@gmail.com>
2023-11-30 11:46:57 +0300

    custom styling selectors

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const heart = document.getElementsByClassName("card");
-container = document.getElementById("container");
+const heart = document.getElementsByClassName("b-card");
+container = document.getElementById("b-container");
 let initialLikes = localStorage.getItem("likes");
-container.querySelector(".likes-count-child").textContent = initialLikes
+container.querySelector(".b-likes-count-child").textContent = initialLikes
 container.addEventListener("click", (e) => {
   let clickedElement = e.target;
   let countElement =
-    clickedElement.parentElement.parentElement.querySelector(".like-count");
+    clickedElement.parentElement.parentElement.querySelector(".b-like-count");
     let countElementChild = 
     clickedElement.parentElement.parentElement.querySelector(
-      ".likes-count-child"
+      ".b-likes-count-child"
     );
 
   if (clickedElement.className == "fa-solid fa-heart") {
